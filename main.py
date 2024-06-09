a6c4b30e7c7be1c457db1e6dcb30bef81e84e4be
Samuel Crisostomo <samueld162@gmail.com>
2024-06-09 12:36:29 -0300

    Trabalhando com bot do Google Sheets

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from ad_controle_financeiro_bot.telegramBot import TelegramBot
+from ad_controle_financeiro_bot.driveBot import DriveBot
 
-bot = TelegramBot()
-bot.start()
+#bot = TelegramBot()
+#bot.start()
+
+driveBot = DriveBot()
+print(driveBot.get_data())
